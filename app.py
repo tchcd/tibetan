58f16ee523d32b4039a7bff2c18b13384e3c1d1e
Petrov Nikita <n.petrov@lucky-team.pro>
2023-03-15 22:46:33 +0300

    change verision on 3.0

--- app.py
+++ app.py
@@ -1,22 +1,26 @@
 from aiogram import Bot, Dispatcher, types
-from aiogram.utils import executor
+from aiogram import executor
 from translate_word import translate_word
-from alphabet import alphabet
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from alphabet import alphabet, send_new_word,check_answer
+from aiogram.fsm.storage.memory import MemoryStorage
 import asyncio
 import config
+from aiogram import Router
+
+def main():
+    bot = Bot(token=config.TOKEN)
+    storage = MemoryStorage()
+    dp = Dispatcher(storage=storage)
+    dp.start_polling(bot)
 
 
-bot = Bot(token=config.TOKEN)
-storage = MemoryStorage()
-dp = Dispatcher(bot, storage=storage)
-loop = asyncio.get_event_loop()
-#db = Database(loop)
 
-
+alphabet(dp, storage)
 translate_word(dp, storage)
-#alphabet(dp)
+
+
 
 # Запуск бота
 if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True)
+    asyncio.run(main())
+
--- translate_word.py
+++ translate_word.py
@@ -2,8 +2,8 @@ import random
 import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.utils import executor
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram import executor
 
 import asyncpg
 import config
@@ -69,8 +69,8 @@ async def send_new_word(message: Message, dp: Dispatcher):
 
     # print(f'DEBUG выбрано слово: {true_word}')
     wrong_words = await get_wrong_translation(true_word)
-    # print(f'DEBUG неверные переводы: {wrong_words}')
-    # print(f'DEBUG истинный перевод: {((true_translation,))}')
+    #print(f'DEBUG неверные переводы: {wrong_words}')
+    #print(f'DEBUG истинный перевод: {((true_translation,))}')
 
     answers = [word[0] for word in list(set(wrong_words + [(true_translation,)]))]
     # print(answers)
@@ -86,6 +86,9 @@ async def send_new_word(message: Message, dp: Dispatcher):
     user_data = dp.current_state(user=message.from_user.id)
     await user_data.set_data({'true_translation': true_translation})
 
+    if message.text.startswith('/'):
+        return
+
 
 async def check_answer(message: Message, dp: Dispatcher):
     # получаем выбранный пользователем вариант ответа
@@ -94,26 +97,21 @@ async def check_answer(message: Message, dp: Dispatcher):
     data = await user_data.get_data()
     correct_answer = data.get('true_translation')
 
-    print('DEBUG ответ юзера:', user_answer)
+    #print('DEBUG ответ юзера:', user_answer)
+    #print('DEBUG корректный ответ:', correct_answer)
     if user_answer == correct_answer:
         await message.answer("Вы выбрали правильный ответ!")
         await send_new_word(message, dp)
     else:
         await message.answer(f"Неправильный ответ! Правильный ответ: {correct_answer}")
 
+    #if message.text.startswith('/'):
+    #    return
 
-async def play_handler(message: Message, dp):
-    await send_new_word(message, dp)
-    await dp.storage.set_state(user=message.from_user.id, state="translate")
-
-
-# def translate_word(dp):
-#     dp.register_message_handler(send_new_word, commands=['start'])
-#     dp.register_message_handler(check_answer)
 
 def translate_word(dp, storage: MemoryStorage):
-    dp.register_message_handler(play_handler, commands=['start'], state="*")
-    dp.register_message_handler(check_answer, state="playing")
+    dp.register_message_handler(lambda message: send_new_word(message, dp), commands=['hello'])
+    dp.register_message_handler(lambda message: check_answer(message, dp))
     dp.storage = storage
 
 
